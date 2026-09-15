7a351cfc3a82c1b0feb0ae1066250309f45ed3d0
Quinton Pang Yi Xuan <87934498+QuintonPang@users.noreply.github.com>
2026-09-15 14:14:23 +0800

    Create 57-insert-interval.java

---
+++ 57-insert-interval.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        if(intervals.length==0) return new int[][]{newInterval};
+        int i = 0;
+        List<int[]> ans = new ArrayList<>();
+       
+        while(i<intervals.length && intervals[i][1] < newInterval[0]){
+            ans.add(intervals[i]);
+            i++;
+        }
+
+
+        while(i<intervals.length && intervals[i][0] <= newInterval[1] ){
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }       
+        
+        
+        ans.add(newInterval);
+
+            
+        while(i<intervals.length){
+            ans.add(intervals[i]);
+            i++;
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
